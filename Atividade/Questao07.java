39c3f125b6331d3f97193bc9deb57819fefe9beb
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-08-17 16:12:09 -0300

    alterações

--- Atividade/Questao07.java
+++ Atividade/Questao07.java
@@ -6,27 +6,18 @@ public class Questao07 {
         
         System.out.println("Cores disponíveis: Verde, Azul, Amarelo, Vermelho");
         System.out.print("Digite a cor da etiqueta do CD: ");
-        String cor = sc.nextLine().toLowerCase();
-        
-        
-        switch (cor) {
-            case "verde":
-                System.out.println("O preço do CD Verde é: R$ 10,00");
-                break; 
-            case "azul":
-                System.out.println("O preço do CD Azul é: R$ 20,00");
-                break;
-            case "amarelo":
-                System.out.println("O preço do CD Amarelo é: R$ 30,00");
-                break;
-            case "vermelho":
-                System.out.println("O preço do CD Vermelho é: R$ 40,00");
-                break;
-            default:
-                
-                System.out.println("Cor inválida! Essa cor não está na tabela de preços.");
-                break;
+        String cor = sc.nextLine();
+
+        if (cor.equals("verde")){
+            System.out.println("Preço do CD verde é: R$ 10,00");
+        }else if(cor.equals("azul")){
+            System.out.println("Preço do CD azul é: R$ 20,00");
+        }else if(cor.equals("amarelo")){
+            System.out.println("Preço do CD amarelo é: R$ 30,00");
+        }else if(cor.equals("vermelho")){
+            System.out.println("Preço do CD vermelho é: R$ 40,00");
         }
+        
         
         sc.close();
     }
